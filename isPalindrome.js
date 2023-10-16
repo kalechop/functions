76589c039718e7e1358b298433b794bf43424572
kalechop <144497542+kalechop@users.noreply.github.com>
2023-10-16 14:05:41 -0700

    Add files via upload

---
+++ isPalindrome.js
@@ -0,0 +1,16 @@
+﻿function isPalindrome(s) {
+    let tempList = [];
+    for (let i = 0; i < s.length; i++) {
+        if (/^[a-zA-Z0-9]+$/.test(s[i])) {
+            tempList.push(s[i].toLowerCase());
+        }
+    }
+    console.log(tempList)
+    let result = true;
+    for (let i = 0; i < tempList.length; i++) {
+        if (tempList[i] != tempList[tempList.length - i - 1]) {
+            result = false;
+        }
+    }
+    return result;
+}
